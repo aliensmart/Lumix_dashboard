e37b6a3f69d7d28d7ecfff51b1cb3edefa668acc
aliensmart <kaoua17@gmail.com>
2022-11-05 08:43:30 +0100

    Sadded terms and conditions sectionS

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -3,7 +3,222 @@ import React from "react";
 const Terms = () => {
   return (
     <div className="contact terms">
-      <div className="contact__container terms_container">Terms</div>
+      <div className="contact__container terms_container">
+        <h1> TERMES ET CONDITIONS GENERALES D’UTILISATION </h1>
+
+        <h3> 1- NOTIONS ET TERMES GENERAUX </h3>
+        <p>
+          {" "}
+          Les règles présentes dans la société Lumix sur la prise des paris et
+          la mise en paiement des gains (les règles ci-après) détermine la
+          particularité des paris, la modalité de la prise des paris de la mise
+          en paiement des gains du règlement des conflits. Les règles présentes
+          régissent tous les autres rapports entre les acteurs de la société ?
+          les règles présentes s’appliquent aux clients du site lumix aussi bien
+          qu’aux clients de toutes les agences de lumix
+        </p>
+        <div>
+          <ul>
+            <li>
+              {" "}
+              <h4> PARI :</h4>
+              <p>
+                {" "}
+                Un accord sur le gain conclus entre le client et lumix au sens
+                des règles établies et dont l’issue dépend de l’évènement au
+                sujet duquel il est inconnu s’il va avoir lieu ou pas. La prise
+                des paris par le client correspond aux termes et conditions
+                préciser par lumix.
+              </p>
+            </li>
+            <li>
+              {" "}
+              <h4> ISSUE :</h4>
+              <p>
+                {" "}
+                Le résultat de l’évènement sur lequel le pari a été effectuer.
+              </p>
+            </li>
+            <li>
+              {" "}
+              <h4> ANNULATION DE PARI :</h4>
+              <p>
+                {" "}
+                Un pari dont le calcul et la mise en paiement ne se réalise pas.
+                Conformément aux conditions présente dans le cas de l’annulation
+                du pari l’accord entre Lumix et le client est considérer comme
+                invalide et ce pari est rembourser.
+              </p>
+            </li>
+          </ul>
+
+          <div>
+            <h3> 2- -DISPOSITION GENERALE </h3>
+            <ol>
+              <li>
+                On accepte que les paris effectuent par les personnes âgées de
+                plus de 21 ans ou ayant l’âge de la majorité prévue par la
+                juridiction du pays du client et acceptant les règles proposées
+                par lumix. En vous inscrivant et en jouant sur le site vous
+                confirmez que vous avez aucune dépendance au jeu.
+              </li>
+              <li>
+                Le droit d’accéder au site et\ou l’utiliser peut-être illégal
+                dans certains pays. Nous attendons que notre site web ne soit
+                pas utiliser pour les paris, les jeux de hasard ou d’autres but
+                par les personnes résidents dans les pays et\ou juridictions ou
+                les activités de ses types sont illégale. Vous devez vous
+                rassurez que lors de la création du compte et\ou l’utilisation
+                du site web vos actions sont légales dans votre juridiction. En
+                plus vous devez garantir que vous avez consulter les avocats
+                avant de vous inscrire sur notre site, si nous découvrons que
+                vous êtes le résident du pays ou l’utilisation du site web est
+                illégal, nous nous réservons le droit de clôturer votre compte
+                et rembourser votre argent que vous avez sur la solde au moment
+                de la clôture (en retirent le gain reçu à partir du moment du
+                dernier dépôt). La société a le droit de ne pas accepter les
+                paris effectuer par le client qui ne respect pas les règles
+                présentées. Une fois inscrit, le client n’a pas le droit de
+                créer un autre compte, un joueur nouveau (en utilisant un autre
+                nom, une autre adresse email…)
+              </li>
+            </ol>
+          </div>
+          <div>
+            <h3> 3- -REGLES GENERALES DE LA PRISE DE PARI</h3>
+            <ol>
+              <li>
+                Le pari est considéré comme pris après son enregistrement sur le
+                serveur et la réception de la confirmation en ligne par le
+                joueur. Tout pari ne peut être ni annulé ni corriger.
+              </li>
+              <li>
+                {" "}
+                Les mises ne peuvent pas excédés le solde courant du compte. Une
+                fois un pari enregistré, le compte est débité après avoir
+                comptabiliser les paris le montant du gain est crédite sur le
+                compte du joueur.
+              </li>
+              <li>
+                le pari est soumis à l’annulation si un joueur à notoirement
+                tromper le personnel en leur accordant une information et des
+                demandes erronées sur la prise de pari
+              </li>
+            </ol>
+          </div>
+
+          <div>
+            <h3>4- -TYPES DE PARIES</h3>
+            <ol>
+              <li>
+                pari simple : est un pari qui concerne uniquement la mise
+                minimale de 500f CFA en une seule prise.
+              </li>
+              <li>
+                pari multiple : est un pari qui consiste à parier plusieurs fois
+                avec le même montant minimal de 500 FCFA qui donne la somme du
+                nombre de pari.{" "}
+              </li>
+              <li>
+                pari combiné : est un pari qui consiste à une mise unique qui
+                dépasse le minimum souhaité.
+              </li>
+            </ol>
+          </div>
+          <div>
+            <h3>5- –DEPOT ET RETRAITS DU COMPTE </h3>
+            <ol>
+              <li>
+                vous pouvez déposer et retirer par différents modes de paiement.
+                Tous les moyens de dépôts et de retraits sont présentés sur la
+                page de paiement.
+              </li>
+              <li>toutes les demandes de retraits sont traités 24h\24.</li>
+              <li>
+                le service de sécurité de lumix a le droit de :
+                <ul>
+                  <li>
+                    Refuser le retrait si le n’est pas utiliser pour jouer avant
+                    de retirer vous devez vérifier votre compte.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                le service de sécurité de lumix déconseille de :
+                <ul>
+                  <li>
+                    Effectuer le transfert entre les systèmes de paiement.
+                  </li>
+                  <li>Déposer et retirer sans parier.</li>
+                </ul>
+                Dans ces cas l’argent sera ré crédité sur votre compte.
+              </li>
+              <li>
+                le retrait n’est possible par les mêmes moyens de paiement de
+                par lesquels vous avez déposé. Au cas où le dépôt a été effectué
+                par les moyens de paiement différents le retrait doit être bien
+                proportionnel à la somme du dépôt.
+              </li>
+              <li>
+                si un utilisateur ne respecte pas les règles de la société (par
+                exemple s’il viol les termes et conditions ne place pas de pari
+                avant le retrait) la société se réserve le droit de refuser le
+                retrait de fond a cet utilisateur.{" "}
+              </li>
+              <li>
+                les comptes de devises (bitcoin) peuvent aussi servir de dépôt
+                et de retrait pour parier.
+              </li>
+              <li>
+                résultat des tirages, dates et heure de leur début règlement du
+                contentieux :
+                <ol>
+                  <li>
+                    Lors des traitements du pari il n’est pris en compte que
+                    l’heure réelle du début de l’évènement qui es défini
+                    approximativement par référence au document officiel des
+                    organisateurs de cet évènement. S’il Ya pas de documents de
+                    ce genre cela se fait en raison de l’information présentées
+                    sur les sites officiels de lumix.
+                  </li>
+                </ol>
+              </li>
+              <li>
+                calcul des gains: Le calcul des gains se fera en fonction du
+                nombre de joueurs par jours et par conséquent les différents
+                gains seront connu au résultat final de chaque échéance.
+              </li>
+              <li>
+                <h5> politique de confidentialité</h5>
+                <p>
+                  L’administration de lumix et ses employés sont
+                  particulièrement attaches à l’importance de la sécurité de vos
+                  données personnelles et de la confidentialité général. Nous
+                  essayons d’être plus transparent à l’égard de la manière dont
+                  nous utilisons vos données et assurons leur protection. Cette
+                  politique de confidentialité défini nos méthodes de traitement
+                  des données et vos options à l’égard de l’utilisation de vos
+                  informations personnelles. Lumix est responsable de vous
+                  fournir des informations claires sur la façon dont nous
+                  utilisons les cookies et nous garantissons aux personnes qui
+                  utilisent notre site web la possibilité de faire le bon choix.
+                  Si vous avez des demandes, des questions et des suggestions
+                  concernant vos informations personnelles ou des questions
+                  générales sur les méthodes de traitement et l’utilisation des
+                  services qui sont fourni, veuillez nous contacter :
+                  +225-01-70-50-60-99. Nous vous demandons également de bien
+                  vouloir nous envoyer a l’adresse (
+                  <a href="mailto:aide@cash-lumiere.com">
+                    aide@cash-lumiere.com
+                  </a>
+                  ) des informations détaillées sur toute page notre site web
+                  dont les cookies configures vous préoccupent.
+                </p>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
